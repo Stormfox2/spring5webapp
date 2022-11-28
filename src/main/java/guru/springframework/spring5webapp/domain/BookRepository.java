32d46b92927a0fd2ef927f384074662adf5bfe63
Quirin Brändli <qbraendli@ketarion.de>
2022-11-28 14:32:54 +0100

    Merge into master

---
+++ src/main/java/guru/springframework/spring5webapp/domain/BookRepository.java
@@ -0,0 +1,6 @@
+package guru.springframework.spring5webapp.domain;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BookRepository extends CrudRepository<Book, Long> {
+}
